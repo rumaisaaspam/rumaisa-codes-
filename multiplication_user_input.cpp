04fdde6069ece5a12cc225badef8dcd72ebd97fd
rumaisaaspam <khanrumaisa991@gmail.com>
2026-03-27 21:26:55 +0500

    Add files via upload
    Simple C++ program for multiplication using user input

---
+++ multiplication_user_input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a;
+cout << "Enter value of a " <<endl;
+cin >> a;
+int b;
+cout<< "Enter value of b" <<endl;
+cin >> b;
+int product=a*b;
+cout << "product of ab is= " << (a*b) <<endl;
+return 0;
+}

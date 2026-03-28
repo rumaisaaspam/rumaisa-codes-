04fc20a95ea04cd9abf2b084de6f9f273551cba4
Rumaisa <khanrumaisa991@gmail.com>
2026-03-28 23:34:09 +0500

    Add files via upload
    Practice question # 2:
    Ahmed visits his bank and makes two seperate deposits into his account on the same day. To keep track of his total deposits without manual calculation errors, he decides to use a C++ program.
    -Takes two deposit amounts as input from the user. 
    - Calculates the total deposited amount.
    -Display the total deposited amount on the screen

---
+++ simple_totaldeposit_user_input.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main (){
+int deposit1,deposit2;
+cout << "enter deposit1" << endl;
+cin >> deposit1;
+cout << "enter deposit2" << endl;
+cin >> deposit2;
+int totaldeposit = deposit1 + deposit2;
+cout << "Your total deposit is" << totaldeposit << endl;
+return 0;
+
+}

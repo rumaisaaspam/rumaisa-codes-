f9a6bc9f1fe0138aa7df594a438c523ecfb72ec7
Rumaisa <khanrumaisa991@gmail.com>
2026-03-28 23:51:43 +0500

    Add files via upload
    Practice problem # 3:
    A shopkeeper wants to calculate the total bill amount  by adding the prices of two purchased items. To avoid manual calculation errors, he decides to write a C++ program.
    Write a C++ program that:
    -Takes two numbers as input (representing the prices of two items)  and, 
    - display there sum as the total bill.

---
+++ simple_totalbill_user_input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main (){
+int price1,price2;
+cout <<"enter the  first product price" <<endl;
+cin >> price1;
+cout <<"enter the second product price" <<endl;
+cin >> price2;
+int totalbill= price1 + price2;
+cout << "Your total bill is " << (totalbill) <<endl;
+return 0;
+}

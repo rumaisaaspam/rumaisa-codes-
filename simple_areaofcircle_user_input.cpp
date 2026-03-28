07cb54e6d6830ccf914267455c95b192736d55d5
Rumaisa <khanrumaisa991@gmail.com>
2026-03-28 21:05:46 +0500

    Add files via upload
    A practice problem # 1:
    A gardener wants to calculate the area of a circular flower bed to know how much grass seed to buy. To avoid mannual mistakes, he decides to use a C++ program. 
    Write a program that:
    -Take radius of the circle as input from the user,
    -Calculate the area of circle using the formula:
            Area=pi*r^2
    -Display the area of the circle on the screen.

---
+++ simple_areaofcircle_user_input.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main (){
+int r;
+cout << "enter the value of radius " <<endl;
+cin >> r;
+
+float pi= 3.14F; //constant value that's why we have written 'F'
+
+cout << "area of a circle is " << (pi*r*r) <<endl;
+return 0;
+}

f43f78125cf3843015f84056fd5e28b54c4d42d1
Rumaisa <khanrumaisa991@gmail.com>
2026-03-28 17:20:15 +0500

    Add files via upload
    Simple C++ program for addition using user input

---
+++ addition_user_input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main (){
+int a;
+cout << "enter value of a" <<endl;
+cin >> a;
+int b;
+cout << "enter value of b" <<endl;
+cin >> b;
+cout << (a+b) <<endl;
+cout << "sum of a+b= " << (a+b) <<endl;
+return 0;
+}

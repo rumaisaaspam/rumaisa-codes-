7eee0887ac8a53628159e074fcefae50c70cb610
Rumaisa <khanrumaisa991@gmail.com>
2026-03-28 17:50:37 +0500

    Add files via upload
    Simple C++ program for division using user input.

---
+++ simple_user_input_division.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main () {
+int a;
+cout << "enter value of a" << endl;
+cin >>a;
+int b;
+cout<< "enter value of b" << endl;
+cin >>b;
+int divide=(a/b);
+cout << "Your answer of a/b is " << (a/b) << endl;
+return 0;
+}
